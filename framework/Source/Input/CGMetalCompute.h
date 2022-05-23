3b28119517b4b59514a477689a6a77ec03703fe1
Jason <JasonBee@163.com>
2022-05-23 11:53:30 +0800

    metal i计算

---
+++ framework/Source/Input/CGMetalCompute.h
@@ -0,0 +1,17 @@
+//
+//  CGMetalCompute.h
+//  CGMetal
+//
+//  Created by Jason on 2022/5/22.
+//
+
+#import <Foundation/Foundation.h>
+#import "CGMetalOutput.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CGMetalCompute : CGMetalOutput
+
+@end
+
+NS_ASSUME_NONNULL_END
